e61700a5f40406b211e331cdcfe5e3dc073f6371
eneko0513 <ion.n@hotmail.co.jp>
2020-03-22 15:06:37 +0900

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -5,3 +5,11 @@ var ownerEditContainerHeader = document.querySelector('.OwnerEditMenuContainer-h
 if (ownerEditContainerHeader == null) {
     alert('投コメ編集画面で起動してください');
 }
+else {
+    // イベントリスナー追加
+    var gridElement = document.getElementById('enquete-placeholder');
+    gridElement.insertAdjacentHTML('afterend', '<div id="commentGrid"><div id="msg">LOADING</div></div>');
+    document.addEventListener('click', function (e) {
+        console.log(e.target);
+    }, false);
+}
