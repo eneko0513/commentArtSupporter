c322ff24cdf4eb2b500c8fbe5bd2825197bdd8d4
eneko0513 <ion.n@hotmail.co.jp>
2020-03-22 15:06:22 +0900

    a

--- ts/index.ts
+++ ts/index.ts
@@ -2,4 +2,11 @@ let ownerEditContainerHeader: Element = document.querySelector('.OwnerEditMenuCo
 // 投コメ画面じゃなければ終わり
 if (ownerEditContainerHeader == null) {
     alert('投コメ編集画面で起動してください')
+} else {
+    // イベントリスナー追加
+    let gridElement: HTMLElement = document.getElementById('enquete-placeholder') as HTMLElement;
+    gridElement.insertAdjacentHTML('afterend', '<div id="commentGrid"><div id="msg">LOADING</div></div>');
+    document.addEventListener('click', function (e: any) {
+        console.log(e.target);
+    }, false);
 }
